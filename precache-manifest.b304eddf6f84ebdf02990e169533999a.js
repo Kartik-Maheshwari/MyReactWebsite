e6b00470efe080e3f255b52b455bce3125367b7d
Kartik Maheshwari <95483381+Kartik-Maheshwari@users.noreply.github.com>
2022-07-26 17:37:17 +0530

    Updates

---
+++ precache-manifest.b304eddf6f84ebdf02990e169533999a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8515e9b4e88bac2d4263",
+    "url": "/MyReactWebsite/static/css/main.5835f9ff.chunk.css"
+  },
+  {
+    "revision": "8515e9b4e88bac2d4263",
+    "url": "/MyReactWebsite/static/js/main.8515e9b4.chunk.js"
+  },
+  {
+    "revision": "38ecc02d009634cff82d",
+    "url": "/MyReactWebsite/static/js/1.38ecc02d.chunk.js"
+  },
+  {
+    "revision": "c62933289e7825d126e6",
+    "url": "/MyReactWebsite/static/js/2.c6293328.chunk.js"
+  },
+  {
+    "revision": "50471dd68fdffd159c9e",
+    "url": "/MyReactWebsite/static/js/runtime~main.50471dd6.js"
+  },
+  {
+    "revision": "f8586d629a7cf12bba59f9caead2f249",
+    "url": "/MyReactWebsite/index.html"
+  }
+];
